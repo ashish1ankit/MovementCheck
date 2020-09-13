c3189140f13a2992ef3b57c7a9bef7cbe482d266
ashish1ankit <32021646+ashish1ankit@users.noreply.github.com>
2020-09-13 20:32:58 +0530

    Second  Commit
    Upload of Unity Project

---
+++ Movement/Assets/Destruccion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruccion : MonoBehaviour {
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Dest"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Dest"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
